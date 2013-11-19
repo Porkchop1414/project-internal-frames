b311491296871e08730eccb5ce10e898e847f12d
Tyler W. Catanzaro <twcxv3@mail.mst.edu>
2013-11-19 14:38:02 -0600

    Comments added

--- src/code/Visualizer.java
+++ src/code/Visualizer.java
@@ -6,11 +6,17 @@ import java.util.*;
 
 public class Visualizer extends JPanel 
 {
+  /**
+    Class that extends JPanel that is responsible for drawing the 
+    data in a graphical form.
+  */
+  //Member Variables
   protected VisualizableSort data;
   protected ArrayList<Integer> list;
 
   public Visualizer(VisualizableSort sort)
   {
+    //Constructor initializes member variables
     data = sort;
     list = new ArrayList<Integer>(sort.getData());
   }
@@ -19,6 +25,9 @@ public class Visualizer extends JPanel
   {
     super.paintComponent(g);
     
+    //For loop that sets the color of graphics to yellow if it was the last
+    //changed index, otherwise blue, and then draws a filled rectangle the
+    //height of the value in the arraylist.
     for(int i = 0; i < list.size(); i++)
     {
       if(Arrays.asList(data.getLatestChanges()).contains(i))
